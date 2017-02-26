880071e77e08e1aacc7e825b2fe7848fb65fb267
mparedes01 <mparedes@uark.edu>
2017-02-26 13:25:32 -0600

    Front Page layout

--- script.js
+++ script.js
@@ -1,47 +1,10 @@
-	// create the module and name it wfd
-	var wfd = angular.module('wfd', ['ngRoute']);
-
-	// configure our routes
-	wfd.config(function($routeProvider) {
-		$routeProvider
-
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
-			})
-
-			// route for the about page
-			.when('/about', {
-				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
-			})
-
-			// route for the contact page
-			.when('/contact', {
-				templateUrl : 'pages/contact.html',
-				controller  : 'contactController'
-			})
-        .when('/login', {
-				templateUrl : 'pages/login.html',
-				controller  : 'loginController'
-			});
-	});
-
-	// create the controller and inject Angular's $scope
-    wfd.controller('loginController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Login Page';
-	});
-	wfd.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Welcome to our Capstone web page application, What\'s for dinner!';
-	});
-
-	wfd.controller('aboutController', function($scope) {
-		$scope.message = 'Look! this is going to be your account page! Thsis will have important information here';
-	});
-
-	wfd.controller('contactController', function($scope) {
-		$scope.message = 'Contact us! JK. This is just a demo.';
-	});
+$('#nav').affix({
+    offset: {
+        top: $('header').height()
+    }
+});
+$('#sidebar').affix({
+    offset: {
+        top: 100
+    }
+});
