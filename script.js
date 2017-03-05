93ec9c1b3e8642510bf8d80d41d663970340daa6
mparedes01 <mparedes@uark.edu>
2017-03-05 17:33:16 -0600

    Search bar

--- script.js
+++ script.js
@@ -9,52 +9,20 @@ app.controller('DropdownCtrl', function ($scope) {
         , "but wait! A third!"
     ];
 });
-app.controller('CarouselDemoCtrl', function ($scope) {
-    $scope.myInterval = 5000;
-    $scope.noWrapSlides = false;
-    $scope.active = 0;
-    var slides = $scope.slides = [];
-    var currIndex = 0;
-    $scope.addSlide = function () {
-        var newWidth = 600 + slides.length + 1;
-        slides.push({
-            image: '/images/food7.png' + newWidth + '/300'
-            , text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4]
-            , id: currIndex++
-        });
-    };
-    $scope.randomize = function () {
-        var indexes = generateIndexesArray();
-        assignNewIndexesToSlides(indexes);
-    };
-    for (var i = 0; i < 4; i++) {
-        $scope.addSlide();
-    }
-    // Randomize logic below
-    function assignNewIndexesToSlides(indexes) {
-        for (var i = 0, l = slides.length; i < l; i++) {
-            slides[i].id = indexes.pop();
+$(function () {
+    $('a[href="#search"]').on('click', function (event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+    $('#search, #search button.close').on('click keyup', function (event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
         }
-    }
-
-    function generateIndexesArray() {
-        var indexes = [];
-        for (var i = 0; i < currIndex; ++i) {
-            indexes[i] = i;
-        }
-        return shuffle(indexes);
-    }
-    // http://stackoverflow.com/questions/962802#962890
-    function shuffle(array) {
-        var tmp, current, top = array.length;
-        if (top) {
-            while (--top) {
-                current = Math.floor(Math.random() * (top + 1));
-                tmp = array[current];
-                array[current] = array[top];
-                array[top] = tmp;
-            }
-        }
-        return array;
-    }
+    });
+    //Do not include! This prevents the form from submitting for DEMO purposes only!
+    $('form').submit(function (event) {
+        event.preventDefault();
+        return false;
+    })
 });
