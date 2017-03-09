ff0a0ae40d5e4adaf7d85af4071d5fe38b9c2a55
mparedes01 <mparedes@uark.edu>
2017-03-08 20:14:24 -0600

    routing

--- app/script.js
+++ app/script.js
@@ -1,28 +1,23 @@
 // script.js
-
-    // create the module and name it scotchApp
-        // also include ngRoute for all our routing needs
-    var MyApp = angular.module('MyApp', ['ngRoute']);
-
-    // configure our routes
-    MyApp.config(function($routeProvider) {
-        $routeProvider
-
-            // route for the home page
-            .when('/app/index.html', {
-                templateUrl : 'home/home.html',
-                controller : 'home.controller'
-            })
-
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
-
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'pages/contact.html',
-                controller  : 'contactController'
-            });
-    });
+// create the module and name it scotchApp
+// also include ngRoute for all our routing needs
+var MyApp = angular.module('MyApp', ['ngRoute']);
+// configure our routes
+MyApp.config(function ($routeProvider) {
+    $routeProvider
+    // route for the home page
+        .when('/', {
+            templateUrl: 'home/home.html'
+            , controller: 'home.controller'
+        })
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'pages/about.html'
+            , controller: 'aboutController'
+        })
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'pages/contact.html'
+            , controller: 'contactController'
+        });
+});
