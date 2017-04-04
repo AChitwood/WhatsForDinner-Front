0ca335758109614ba7686edf7bbbdcad98cbc5fe
jenlisbeth <Jenifer@Jenifers-MacBook-Pro.local>
2017-04-04 16:04:45 -0500

    Remove message

--- app/server.js
+++ app/server.js
@@ -16,8 +16,3 @@ app.get('/', function (req, res) {
     // listen (start app with node server.js) ======================================
 app.listen(3000);
 console.log("App listening on port 3000");
-app.get('/', function (req, res) {
-    res.json({
-        Hello: 'whats up?'
-    });
-})
