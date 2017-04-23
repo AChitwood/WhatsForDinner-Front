5863d7582c286ea964be04b346045ad1bb537f31
mparedes01 <mparedes@uark.edu>
2017-04-22 21:28:26 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 14fcfb889837fd3bb7c21a3280389b88b0d9d424, reversing
    changes made to e305f17322e33c2764bf6843ca0d7e659b75448d.

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,36 +1,11 @@
-(function () {
-    'use strict';
-    angular.module('app').controller('HomeController', HomeController);
-    HomeController.$inject = ['UserService', '$rootScope'];
-
-    function HomeController(UserService, $rootScope) {
-        var vm = this;
-        vm.user = null;
-        vm.allUsers = [];
-        vm.deleteUser = deleteUser;
-        initController();
-
-        function initController() {
-            loadCurrentUser();
-            loadAllUsers();
-        }
-
-        function loadCurrentUser() {
-            UserService.GetByUsername($rootScope.globals.currentUser.username).then(function (user) {
-                vm.user = user;
-            });
-        }
-
-        function loadAllUsers() {
-            UserService.GetAll().then(function (users) {
-                vm.allUsers = users;
-            });
-        }
-
-        function deleteUser(id) {
-            UserService.Delete(id).then(function () {
-                loadAllUsers();
-            });
-        }
-    }
-})();
+//var app = angular.module('homepage', ['ui.bootstrap']);
+//app.controller('MainCtrl', function ($scope) {
+//    $scope.name = 'World';
+//});
+//app.controller('DropdownCtrl', function ($scope) {
+//    $scope.items = [
+//        "The first choice!"
+//        , "And another choice for you."
+//        , "but wait! A third!"
+//    ];
+//});
