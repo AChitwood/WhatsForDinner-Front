f060c6792c031ebd1e4a34f26a34702bdd25301f
mparedes01 <mparedes@uark.edu>
2017-04-26 23:27:27 -0500

    object boxes created

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,11 +1,12 @@
 (function () {
     'use strict';
     angular.module('app').controller('HomeController', HomeController);
-    HomeController.$inject = ['UserService', '$rootScope'];
+    HomeController.$inject = ['UserService', '$rootScope', '$scope'];
 
-    function HomeController(UserService, $rootScope) {
+    function HomeController(UserService, $rootScope, $scope) {
         var vm = this;
         vm.user = null;
+        vm.names = null;
         vm.allUsers = [];
         vm.deleteUser = deleteUser;
         initController();
@@ -13,6 +14,59 @@
         function initController() {
             loadCurrentUser();
             loadAllUsers();
+            testName();
+        }
+
+        function testName() {
+            vm.names = [
+                {
+                    "Name": "Veggie Burger"
+                    , "image": "Germany"
+    }
+                , {
+                    "Name": "Smoked Salmon"
+                    , "image": "Sweden"
+    }
+                , {
+                    "Name": "Oven Baked Chicken"
+                    , "image": "Mexico"
+    }
+                , {
+                    "Name": "Raspberry Custard"
+                    , "image": "Austria"
+    }
+                , {
+                    "Name": "Chicken Parmasean"
+                    , "image": "Austria"
+    }
+                , {
+                    "Name": "Juicy Burger"
+                    , "image": "Austria"
+    }, {
+                    "Name": "Zesty Salad"
+                    , "image": "Austria"
+    }
+                , {
+                    "Name": "Strawberry Parfait"
+                    , "image": "Austria"
+    }
+                , {
+                    "Name": "Chocolate Layered Cake"
+                    , "image": "Austria"
+    }
+                , {
+                    "Name": "Berry Waffles"
+                    , "image": "Austria"
+    }
+                , {
+                    "Name": "Popcorn Chicken"
+                    , "image": "Austria"
+    }
+                , {
+                    "Name": "Spaghetti"
+                    , "image": "Austria"
+    }
+  ];
         }
 
         function loadCurrentUser() {
