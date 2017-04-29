d36be895aac077cba3d7784b8344801438d289a6
mparedes01 <mparedes@uark.edu>
2017-04-29 10:51:41 -0500

    hjk

--- app/search/search.controller.js
+++ app/search/search.controller.js
@@ -13,11 +13,24 @@
             recipes();
         }
 
+        function test(variable) {
+            //            var stringArray = variable.split(",")
+            //            for (int = 0; i < variable.length; i++) {
+            //                if (i != 0) {
+            //                    //add stuff
+            //                    variable[i]
+            //                }
+            //            }
+            //            return variable;
+            alert(hello);
+        }
+
         function recipes() {
             vm.recipe = [
                 {
                     "Name": "Veggie Burger"
-                    , "description": "Low-calorie veggie burger"
+                    , "Ingredients": "2 cans(15 oz each) Rosarita® Premium Whole Black Beans, drained, rinsed 1 cup panko bread crumbs 1 / 2 cup Egg Beaters® Original 1 - 1 / 2 teaspoons Cajun seasoning 1 / 4 teaspoon salt 2 tablespoons Pure Wesson® Canola Oil, divided 6 tablespoons guacamole 6 whole wheat hamburger buns 6 slices fresh tomato Purchase ingredients from Peapod or Amazon"
+                    , "Directions": "Mash black beans with potato masher in medium bowl until almost smooth.Add bread crumbs, Egg Beaters, Cajun seasoning and salt;mix until blended.Shape mixture into 6 patties, about 1 / 2 - inch thick. Heat 1 tablespoon oil in large skillet over medium - high heat.Add 3 patties;cook 3 minutes on each side or untilset and hot.Remove from skillet;keep warm.Add remaining 1 tablespoon oil and repeat with remaining patties.Remove from heat. Spread 1 tablespoon guacamole on bottom half of each bun.Top each with a patty and 1 tomato slice.Close with tops of buns.Serve immediately."
                     , "image": "/app/assets/images/burger.jpeg"
                 }, {
                     "Name": "Smoked Salmon"
