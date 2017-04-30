c5df3e88e5fca9c7b8768c3547b7d857af3125c1
mparedes01 <mparedes@uark.edu>
2017-04-30 10:12:56 -0500

    menu change

--- app/profile/profile.controller.js
+++ app/profile/profile.controller.js
@@ -1,9 +1,9 @@
 (function () {
     'use strict';
     angular.module('app').controller('ProfileController', ProfileController);
-    ProfileController.$inject = ['UserService', '$rootScope'];
+    ProfileController.$inject = ['UserService', '$rootScope', '$scope'];
 
-    function ProfileController(UserService, $rootScope) {
+    function ProfileController(UserService, $rootScope, $scope) {
         var vm = this;
         vm.user = null;
         vm.allUsers = [];
@@ -13,7 +13,10 @@
         function initController() {
             loadCurrentUser();
             loadAllUsers();
+            testName();
         }
+
+        function directive() {}
 
         function loadCurrentUser() {
             UserService.GetByUsername($rootScope.globals.currentUser.username).then(function (user) {
@@ -31,6 +34,59 @@
             UserService.Delete(id).then(function () {
                 loadAllUsers();
             });
+        }
+
+        function testName() {
+            vm.names = [
+                {
+                    "Name": "Veggie Burger"
+                    , "description": "Low-calorie veggie burger"
+                    , "image": "/app/assets/images/burger.jpeg"
+                }, {
+                    "Name": "Smoked Salmon"
+                    , "description": "Quick and easy to do smoke salmon"
+                    , "image": "/app/assets/images/fish.jpeg"
+                }, {
+                    "Name": "Oven Baked Chicken"
+                    , "description": "Southern-style oven baked chicken"
+                    , "image": "/app/assets/images/food2.jpg"
+                }, {
+                    "Name": "Raspberry Custard"
+                    , "description": "Craving dessert?"
+                    , "image": "/app/assets/images/food4.jpg"
+                }, {
+                    "Name": "Chicken Parmasean"
+                    , "description": "Love chicken, pasta, and cheese?"
+                    , "image": "/app/assets/images/food6.jpg"
+                }, {
+                    "Name": "Juicy Burger"
+                    , "description": "The name says it all..."
+                    , "image": "/app/assets/images/juicyburger.jpeg"
+                }, {
+                    "Name": "Zesty Salad"
+                    , "description": "Trying to stay healthy?"
+                    , "image": "/app/assets/images/salad.jpg"
+                }, {
+                    "Name": "Strawberry Parfait"
+                    , "description": "Make a delicious pastry for the family"
+                    , "image": "/app/assets/images/sweet1.jpg"
+                }, {
+                    "Name": "Chocolate Layered Cake"
+                    , "description": "Need chocolate?"
+                    , "image": "/app/assets/images/sweets2.jpeg"
+                }, {
+                    "Name": "Berry Waffles"
+                    , "description": "Easy to do berry waffles"
+                    , "image": "/app/assets/images/waffles.jpg"
+                }, {
+                    "Name": "Popcorn Chicken"
+                    , "description": "This chicken is popping"
+                    , "image": "/app/assets/images/popcornChick.jpg"
+                }, {
+                    "Name": "Spaghetti"
+                    , "description": "Craving Italian? No need to go to Italy"
+                    , "image": "/app/assets/images/spagetti.jpg"
+                }];
         }
     }
 })();
